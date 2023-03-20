02a91b73df8358fe4ca44441fea2d5c3087f77d6
Wangxin555 <wangx555@mail2.sysu.edu.cn>
2023-03-19 20:38:08 -0400

    Set url for base html

--- Yummy/urls.py
+++ Yummy/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 from Yummy import views
+from django.urls import path
+
 
 urlpatterns = [
     path('', views.global_action),
-    path('admin/', admin.site.urls),
-    path('yummy/', include('Yummy.urls')),
+    #path('login', views.login_action, name='login'),
+    #path('register', views.register_action, name='register'),
 ]
