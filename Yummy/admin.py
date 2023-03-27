69601a9f211beaf6f17e7bd650e1e5c45a7cd922
kylieeeeee <kelly97491234@gmail.com>
2023-03-27 16:38:14 -0400

    Updated permission for adding new dish

--- Yummy/admin.py
+++ Yummy/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.urls import path
 
-# Register your models here.
+from Yummy.models import *
+from Yummy.forms import FoodForm
+
+
+# # Register your models here.
+admin.site.register(Food)
+admin.site.register(FoodPicture)
+admin.site.register(Profile)
+
+
