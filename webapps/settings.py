43d722633a588accc7e9b048658f6a1ea59f1048
Ubuntu <ubuntu@ip-172-31-38-51.us-east-2.compute.internal>
2023-04-14 18:12:40 +0000

    make modification on the sever settings

--- webapps/settings.py
+++ webapps/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'webapps.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+'default': {
+    'OPTIONS': {'charset': 'utf8mb4'},
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'django',
+    'USER': '',
+    'PASSWORD': '',
     }
 }
 
